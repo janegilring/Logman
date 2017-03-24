a4b66f26c4fb05b74aeba9b69d7edc080f933afd
Ben Gelens <ben@bgelens.nl>
2017-03-24 11:59:08 +0100

    added logfilepath parameter and logic

--- Logman.psm1
+++ Logman.psm1
@@ -8,13 +8,19 @@ enum Ensure
 class Logman
 {
     [DscProperty(Key)]
+    [ValidateNotNullOrEmpty()]
     [string] $DataCollectorSetName
 
     [DscProperty(Mandatory)]
     [Ensure] $Ensure
 
-    [DscProperty(Mandatory)]
+    [DscProperty()]
+    [ValidateNotNullOrEmpty()]
     [string] $XmlTemplatePath
+
+    [DscProperty()]
+    [ValidateNotNullOrEmpty()]
+    [string] $LogFilePath
 
     [Logman] Get()
     {
@@ -22,7 +28,8 @@ class Logman
 
         $returnObj = [Logman]::new()
         $returnObj.DataCollectorSetName = $this.DataCollectorSetName
-        $returnObj.XmlTemplatePath      = $this.XmlTemplatePath
+        $returnObj.XmlTemplatePath  = $this.XmlTemplatePath
+        $returnObj.LogFilePath = $this.GetLogPath()
 
         if ($logmanquery -eq $this.DataCollectorSetName) 
         {
@@ -38,20 +45,32 @@ class Logman
 
     [void] Set()
     {
-        if( $this.Ensure -eq [Ensure]::Present)
-        {
-            if (Test-Path -Path $this.XmlTemplatePath) 
-            {
-                Write-Verbose -Message "Importing logman Data Collector Set $($this.DataCollectorSetName) from Xml template $($this.XmlTemplatePath)"
+        $logmanquery = $this.Query()
 
-                $null = logman.exe import -n $this.DataCollectorSetName -xml $this.XmlTemplatePath
-            }
-            else 
+        if($this.Ensure -eq [Ensure]::Present)
+        {
+            if ($logmanquery -eq [string]::Empty -and $this.XmlTemplatePath)
             {
-                Write-Verbose -Message "$($this.XmlTemplatePath) not found or temporary inaccessible, trying again on next consistency check"
+                if (Test-Path -Path $this.XmlTemplatePath) 
+                {
+                    Write-Verbose -Message "Importing logman Data Collector Set $($this.DataCollectorSetName) from Xml template $($this.XmlTemplatePath)"
+
+                    $null = logman.exe import -n $this.DataCollectorSetName -xml $this.XmlTemplatePath
+                }
+                else 
+                {
+                    Write-Verbose -Message "$($this.XmlTemplatePath) not found or temporary inaccessible, trying again on next consistency check"
+                }
+            }
+
+            $CurrentLogPath = $this.GetLogPath()
+            if ($CurrentLogPath -ne $this.LogFilePath)
+            {
+                Write-Verbose -Message "Updating LogFilePath $CurrentLogPath to $($this.LogFilePath)"
+                $this.UpdateLogPath()
             }
         }
-        elseif ($this.Ensure -eq [Ensure]::Absent) 
+        else
         {
             Write-Verbose -Message "Removing logman Data Collector Set $($this.DataCollectorSetName)"
             $null = logman.exe delete $this.DataCollectorSetName
@@ -65,6 +84,13 @@ class Logman
         if ($logmanquery -eq $this.DataCollectorSetName) 
         {
             Write-Verbose -Message "Data Collector $($this.DataCollectorSetName) exists"
+
+            
+            if ($this.LogFilePath -and $this.LogFilePath -ne $this.GetLogPath())
+            {
+                Write-Verbose -Message "LogFilePath is not configured correctly"
+                return $false
+            }
 
             if ($this.Ensure -eq [Ensure]::Present) 
             {
@@ -93,7 +119,21 @@ class Logman
     [string] Query ()
     {
         $logmanquery = (logman.exe query $this.DataCollectorSetName |
-            Select-String -Pattern "^Name:\s*$($this.DataCollectorSetName)$") -replace "^Name:\s*",',' -as [string]
+            Select-String -Pattern "^Name:\s*$($this.DataCollectorSetName)$") -replace "^Name:\s*",'' -as [string]
         return $logmanquery
+    }
+
+    [string] GetLogPath ()
+    {
+        $logmanquery = ((logman.exe query $this.DataCollectorSetName |
+            Select-String -Pattern "^Output\sLocation:\s*") -replace "^Output\sLocation:\s*",''  -split '_')[0] -as [string]
+        return $logmanquery
+    }
+
+    [void] UpdateLogPath ()
+    {
+        logman.exe -stop $this.DataCollectorSetName
+        logman.exe -update $this.DataCollectorSetName -o "$($this.LogFilePath)"
+        Logman.exe -start $this.DataCollectorSetName
     }
 }
